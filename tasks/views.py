9570fd8365d31291b0b68233882e4b1d0c9c6a6f
Evanderss <evanderramirez150@gmail.com>
2022-09-21 19:10:08 -0500

    Registrando usuarios, comparando contraseñas y mensajes de error en caso de usuarios existentes

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 # Create your views here.
-def helloWorld(request):
-    return render(request, "signup.html", {
-        "form" : UserCreationForm 
+def home(request):
+    return render(request, "home.html")
+
+def singup(request):
+    if request.method == "GET":
+        print("enviando formulario")
+        return render(request, "signup.html", {
+            "form" : UserCreationForm 
     } )
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            #register user
+            try:
+                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user.save()
+                return HttpResponse("Usario creado correctamente")
+            except: 
+                return HttpResponse("El usuario ya existe")
+        return HttpResponse("La contraseña no coincide")

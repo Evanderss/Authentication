d1cad193b7e91141b7c3d502736c60ab6f013918
Evanderss <evanderramirez150@gmail.com>
2022-09-22 23:18:33 -0500

    Terminando vista principal, añadiendo funcionalidad para usuarios repetidos, redireccionando a tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 # Create your views here.
 def home(request):
     return render(request, "home.html")
@@ -18,8 +19,9 @@ def singup(request):
             try:
                 user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
                 user.save()
-                return HttpResponse("Usario creado correctamente")
-            except: 
+                login(request, user)
+                return redirect("tasks")
+            except IntegrityError: 
                 return render(request, "signup.html", {
                     "form" : UserCreationForm ,
                     "error" : "El usuario ya existe"
@@ -28,3 +30,6 @@ def singup(request):
             "form" : UserCreationForm ,
             "error" : "Las contraseñas no coinciden"
     })
+
+def tasks(request):
+    return render(request, "tasks.html") 

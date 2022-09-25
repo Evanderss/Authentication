22c64fb7757068102453e966e02945e1521266cc
Evanderss <evanderramirez150@gmail.com>
2022-09-25 16:36:10 -0500

    Haciendo funcionar logout y redireccionando al home

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 # Create your views here.
 def home(request):
@@ -33,3 +33,7 @@ def singup(request):
 
 def tasks(request):
     return render(request, "tasks.html") 
+
+def singout(request):
+    logout(request)
+    return redirect("home")

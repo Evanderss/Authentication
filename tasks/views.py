7101a11c48fc078ca9f76ea97a746fef2c28284c
Evanderss <evanderramirez150@gmail.com>
2022-11-05 20:30:57 -0600

    Creando la funcion para eeeel create

--- tasks/views.py
+++ tasks/views.py
@@ -3,17 +3,17 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .models import Task
 from .forms import TaskForm
-# Create your views here.
+
 def home(request):
     return render(request, "home.html")
+
 
 def singup(request):
     if request.method == "GET":
         print("enviando formulario")
-        return render(request, "signup.html", {
-            "form" : UserCreationForm 
-    } )
+        return render(request, "signup.html", {"form" : UserCreationForm })
     else:
         if request.POST["password1"] == request.POST["password2"]:
             #register user
@@ -23,45 +23,41 @@ def singup(request):
                 login(request, user)
                 return redirect("tasks")
             except IntegrityError: 
-                return render(request, "signup.html", {
-                    "form" : UserCreationForm ,
-                    "error" : "El usuario ya existe"
-                })
-        return render(request, "signup.html", {
-            "form" : UserCreationForm ,
-            "error" : "Las contraseñas no coinciden"
-    })
+                return render(request, "signup.html", {"form" : UserCreationForm,"error" : "El usuario ya existe"})
+        return render(request, "signup.html", {"form" : UserCreationForm,"error" : "Las contraseñas no coinciden"})
 
 
 def tasks(request):
-    return render(request, "tasks.html") 
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "tasks.html", {"tasks": tasks}) 
 
-def create_tas(request):
+
+def create_task(request):
     if request.method == "GET":
-        return render(request, "create_tas.html", {
-        "form": TaskForm
-        })
-    else: 
-        print(request.POST)
-        return render(request, "create_tas.html", {
-        "form": TaskForm
-        })
+        return render(request, 'create_task.html', {"form": TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
+
 
 def signout(request):
     logout(request)
     return redirect("home")
 
+
 def signin(request):
     if request.method == "GET":
-        return render(request, "signin.html", {
-        "form": AuthenticationForm
-    })
+        return render(request, "signin.html", {"form": AuthenticationForm})
     else: 
         user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
         if user is None:
-            return render(request, "signin.html", {
-            "form": AuthenticationForm,
-            "error": "Username or password is incorrect"})
+            return render(request, "signin.html", {"form": AuthenticationForm,"error": "Username or password is incorrect"})
         else:
             login(request, user)
             return redirect("tasks")

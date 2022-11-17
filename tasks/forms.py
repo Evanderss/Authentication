678c856863b60c5ac0945c03c894838f6f22a892
Evanderss <evanderramirez150@gmail.com>
2022-11-17 00:09:08 -0600

    Empezando a estilizar desde forms sin tocar directamente el html

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "important"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"})
+        }
